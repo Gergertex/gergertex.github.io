0a97f6f91493f230ac6bdcd56980f57698fd7045
Gergertex <55833589+Gergertex@users.noreply.github.com>
2020-01-17 00:25:20 +0100

    Add files via upload

---
+++ LandingSVG/script.js
@@ -0,0 +1,100 @@
+$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});$("#Layer_1").click( function() {
+
+	 var mySVG = $('#Layer_1').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});// JavaScript Document
+// JavaScript Document
+
+$(window).scroll( function() {
+	if ( $('#Layer_3').visible(true) ) {
+		
+	  var mySVG = $('#Layer_3').drawsvg();
+    mySVG.drawsvg('animate');	
+    
+	}
+});
+
+
+
+$("#Layer_3").click( function() {
+
+	 var mySVG = $('#Layer_3').drawsvg();
+   mySVG.drawsvg('animate');	
+   
+});
